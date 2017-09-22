500b71e0f7e02e9521637f3c7aebacd165e056f9
Ryan Marinoff <ryan.marinoff@outlook.com>
2017-09-22 10:44:59 -0400

    Section 4 Lesson 122
    Setup logging and control of PlayerController code in new custom class TankPlayerController

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -7,4 +7,20 @@ ATank* ATankPlayerController::GetControlledTank() const
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	auto ControlledTank = GetControlledTank();
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("PlayerController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *ControlledTank->GetName());
+	}
+}
