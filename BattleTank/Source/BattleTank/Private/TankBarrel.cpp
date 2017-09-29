cb1c876e5335140204563097aeaa22bde4ac439d
Ryan Marinoff <ryan.marinoff@outlook.com>
2017-09-29 10:49:27 -0400

    Revert "Section 4 Lecture 142"
    This reverts commit ab47c0c93243b566207010e7ad1341a8e660653e.

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -2,20 +2,14 @@
 
 #include "TankBarrel.h"
 
-#include "Engine/World.h"
-#include "Math/UnrealMathUtility.h"
+//#include "Engine/World.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
 
 	// Move the barrel the right amount this frame
 	// Given a max elevation speed, and the frame time
-	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1.0f, 1.0f);
-	float ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
-	float RawNewElevation = RelativeRotation.Pitch + ElevationChange;
-	float Elevation = FMath::Clamp<float>(RawNewElevation, MinDegreeElevation, MaxDegreeElevation);
-
-	UE_LOG(LogTemp, Warning, TEXT("Move Rotation %f"), RawNewElevation)
-	SetRelativeRotation(FRotator(Elevation, 0, 0));
+	//auto Time = GetWorld()->GetTimeSeconds();
+	//UE_LOG(LogTemp, Warning, TEXT("%f: Barrel-Elevate() called at speed %f"), Time, RelativeSpeed)
 }
 
