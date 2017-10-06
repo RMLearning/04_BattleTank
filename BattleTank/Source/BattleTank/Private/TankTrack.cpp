f4dcd9a2f2f2a4b3c1e5bc0944aca6a42b4bead8
Ryan Marinoff <ryan.marinoff@outlook.com>
2017-10-06 10:56:22 -0400

    Section 4 Lecture 155
    ApplyForceAtLocation() in Action
    Applied a force to the tank for movement. Not enough force is currently applied, or not the correct variables are set.

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -3,11 +3,18 @@
 #include "TankTrack.h"
 #include "Engine/World.h"
 
+#include "Components/PrimitiveComponent.h"
+
 void UTankTrack::SetThrottle(float Throttle)
 {
 	auto Time = GetWorld()->GetTimeSeconds();
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%f: Track %s Throttle at %f."), Time, *Name, Throttle)
+	
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
 
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -15,8 +15,11 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 	GENERATED_BODY()
 	
 public:
+	// Sets a throttle between -1 and +1
 	UFUNCTION(BlueprintCallable, Category = "Input")
 	void SetThrottle(float Throttle);
 	
-	
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	float TrackMaxDrivingForce = 400000; // assume 40t mass tank, 1g acceleration
 };
