501449b01cfebff433b42c376c582c8fdd02e162
Ryan Marinoff <ryan.marinoff@outlook.com>
2017-10-10 11:39:13 -0400

    Section 4 Lecture 159
    A Better Component Architecture
    Moved the movement component from being a blueprint default to a blueprint callable component, changed the blueprint to reflect this change, and "wired up" the tank tracks to the movement component within code.

--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -2,10 +2,20 @@
 
 #include "TankMovementComponent.h"
 
+#include "TankTrack.h"
 #include "Engine/World.h"
+
+void UTankMovementComponent::Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+}
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	auto Time = GetWorld()->GetTimeSeconds();
 	UE_LOG(LogTemp, Warning, TEXT("%f: Throw Value at %f."), Time, Throw)
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
 }
